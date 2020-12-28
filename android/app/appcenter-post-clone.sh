60d09c674d1922a10d7cef4905fd21d55bd335f7
Henrik <henrikthienth@gmail.com>
2020-12-28 16:46:03 +0100

    added flutter appcenter post script

---
+++ android/app/appcenter-post-clone.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# place this script in project/android/app/
+cd ..
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+cd ..
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
+# stable - recommended for production
+
+git clone -b stable https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter channel stable
+flutter doctor
+
+flutter pub get
+
+flutter pub run build_runner build --delete-conflicting-outputs
+
+flutter build apk --release
+flutter build appbundle
+
+# copy the APK where AppCenter will find it
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+
+#copy aab
+mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
